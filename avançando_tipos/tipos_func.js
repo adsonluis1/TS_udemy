19aafff936a557537d3a997ce1e26b9565eb8ac2
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-08 20:51:20 -0300

    finalizando tipos_func.ts

---
+++ avançando_tipos/tipos_func.js
@@ -0,0 +1,6 @@
+// tipando o return de uma função
+function suadacao(nome) {
+    return "ol\u00E1 ".concat(nome);
+}
+// no ex de cima estou obrigando que a função 'saudacao' volte um string
+console.log(suadacao('adson'));
---
+++ avançando_tipos/tipos_func.ts
@@ -0,0 +1,7 @@
+// tipando o return de uma função
+
+function suadacao(nome:string):string{
+    return `olá ${nome}`
+}
+// no ex de cima estou obrigando que a função 'saudacao' volte um string
+console.log(suadacao('adson'))
