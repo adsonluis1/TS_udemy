7b58a42585ad893ce1034497e6c6fbdf2ec287c0
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-08 19:41:52 -0300

    number.ts

---
+++ fundamentos/number.js
@@ -0,0 +1,8 @@
+// aula 1
+// tipando a string antes de dar o valor
+var num;
+num = 10;
+var num2 = 30;
+// num = 'adson'
+// variaveis do tipo number nn pode ter valor do tipo string
+console.log(num + num2);
---
+++ fundamentos/number.ts
@@ -0,0 +1,12 @@
+// aula 1
+// tipando a string antes de dar o valor
+let num:number;
+
+num = 10;
+
+const num2:number = 30;
+
+// num = 'adson'
+// variaveis do tipo number nn pode ter valor do tipo string
+
+console.log(num + num2);
