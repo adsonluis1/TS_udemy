75c5d8a6ac1387ca53ea60a233b4539c26e9571f
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-08 20:39:36 -0300

    finalizando any.ts

---
+++ avançando_tipos/any.js
@@ -0,0 +1,9 @@
+// (não usar ) ele faz com que o ts libere qualquer tipo para determinada variavel
+var num = 10;
+console.log(num);
+num = true;
+console.log(num);
+num = 'anne';
+console.log(num);
+num = ['adson', 30, false];
+console.log(num);
