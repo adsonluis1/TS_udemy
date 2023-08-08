a37002136f4f7bd55ab41ca0a5761092c603b580
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-08 20:01:47 -0300

    finalizando ann_inf.ts

---
+++ fundamentos/ann_inf.ts
@@ -0,0 +1,12 @@
+// aula 2
+
+//esses são os 2 jeitos de declarar um tipo anotando ou deixando o ts identificar o tipo
+
+
+let ann:string ='Adson'
+
+// no ex de cima fiz a tipagem manualmente para string
+
+let inf = 'adson'
+
+// no ex de cima o proprio ts viu que o valor é string e ja fez a tipagem (não posso mudar)
