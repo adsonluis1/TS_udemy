349ab892eac3d6dcba0c35923a48dfece95631f3
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-08 20:16:32 -0300

    finalizando t2

---
+++ tarefas/t2.js
@@ -0,0 +1,4 @@
+var number = 18;
+var string_number = number.toString();
+var Print = "eu tenho ".concat(string_number, " anos'");
+console.log(Print);
---
+++ tarefas/t2.ts
@@ -0,0 +1,7 @@
+const number:number= 18
+
+let string_number:string = number.toString()
+
+const Print:string = `eu tenho ${string_number} anos'`
+
+console.log(Print)
