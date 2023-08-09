fb9cf231e14faa2fe3afef943c763ca3eecd2c2c
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-09 19:28:21 -0300

    dom em ts

---
+++ avançando_tipos/domEmTs.ts
@@ -0,0 +1,7 @@
+const p = document.getElementById('tela-p')
+
+console.log(p!.innerText)
+
+p!.style.color= 'blue'
+
+// o operators '!' faz com que cale o erro
---
+++ avançando_tipos/js/domEmTs.js
@@ -0,0 +1,4 @@
+var p = document.getElementById('tela-p');
+console.log(p.innerText);
+p.style.color = 'blue';
+// o operators '!' faz com que cale o erro
