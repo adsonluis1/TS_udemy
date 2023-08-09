238344231a044143f4374032725a4f444eae5342
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-09 18:18:47 -0300

    validação variaveis opcionais

--- avançando_tipos/variaveisopcionais.js
+++ avançando_tipos/variaveisopcionais.js
@@ -6,3 +6,12 @@ function mostrar(n1, n2, n3) {
 // variavel com o ? antes do tipo significa que essa variavel não é obrigatoria
 mostrar('adson', 'anne', 'pedro');
 mostrar('adson');
+// validação
+function ola(primeirNome, segundoNome) {
+    if (segundoNome !== undefined) {
+        return "Ol\u00E1 ".concat(primeirNome, " ").concat(segundoNome, ", como vai ?");
+    }
+    return "Ol\u00E1 ".concat(primeirNome, ", como vai ?");
+}
+console.log(ola('adson', 'luis'));
+console.log(ola('adson'));
--- avançando_tipos/variaveisopcionais.ts
+++ avançando_tipos/variaveisopcionais.ts
@@ -9,3 +9,17 @@ function mostrar(n1:string, n2?:string , n3?:string){
 mostrar('adson' , 'anne' , 'pedro')
 
 mostrar('adson')
+
+// validação
+
+function ola(primeirNome:string,segundoNome?:string){
+    if(segundoNome !== undefined){
+        return `Olá ${primeirNome} ${segundoNome}, como vai ?`
+    }
+
+    return `Olá ${primeirNome}, como vai ?`
+}
+
+console.log(ola('adson' , 'luis'))
+
+console.log(ola('adson'))
