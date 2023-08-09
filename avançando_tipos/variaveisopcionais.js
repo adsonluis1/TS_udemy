208c8d753170803f42ad916eb0d67271ee2b3740
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-09 18:11:16 -0300

    finalizando variavelopcionais.ts

---
+++ avançando_tipos/variaveisopcionais.js
@@ -0,0 +1,8 @@
+function mostrar(n1, n2, n3) {
+    console.log("PRIMEIRO NOME: ".concat(n1));
+    console.log("SEGUNDO NOME: ".concat(n2));
+    console.log("TERCEIRO NOME: ".concat(n3));
+}
+// variavel com o ? antes do tipo significa que essa variavel não é obrigatoria
+mostrar('adson', 'anne', 'pedro');
+mostrar('adson');
---
+++ avançando_tipos/variaveisopcionais.ts
@@ -0,0 +1,11 @@
+function mostrar(n1:string, n2?:string , n3?:string){
+    console.log(`PRIMEIRO NOME: ${n1}`)
+    console.log(`SEGUNDO NOME: ${n2}`)
+    console.log(`TERCEIRO NOME: ${n3}`)
+}
+
+// variavel com o ? antes do tipo significa que essa variavel não é obrigatoria
+
+mostrar('adson' , 'anne' , 'pedro')
+
+mostrar('adson')
