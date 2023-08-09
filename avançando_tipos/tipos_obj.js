64efc1cefb5e07830eb6d649a6ee23a9dfeeecf7
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-08 21:01:33 -0300

    finalizando tipos_obj.ts

---
+++ avançando_tipos/tipos_obj.js
@@ -0,0 +1,5 @@
+function position(pos) {
+    console.log("x = ".concat(pos.x));
+    console.log("y = ".concat(pos.y));
+}
+position({ x: 10, y: 3090 });
---
+++ avançando_tipos/tipos_obj.ts
@@ -0,0 +1,7 @@
+function position(pos:{x:number , y:number}){
+    console.log(`x = ${pos.x}`)
+    console.log(`y = ${pos.y}`)
+}
+
+position({x:10 , y:3090})
+
