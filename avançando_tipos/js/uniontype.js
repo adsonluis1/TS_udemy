bed9607979dce146cb9506da75f1457b0d355aa2
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-09 18:27:23 -0300

    union type

---
+++ avançando_tipos/js/uniontype.js
@@ -0,0 +1,7 @@
+// é um jeito de fzr com que a string aceite mias de um tipo
+function retornarSaldo(saldo) {
+    return saldo;
+}
+// no exemplo de cima a variavel saldo aceita tanto o tipo string como o tipo number
+console.log(retornarSaldo(5000));
+console.log(retornarSaldo('5000'));
---
+++ avançando_tipos/uniontype.ts
@@ -0,0 +1,11 @@
+// é um jeito de fzr com que a string aceite mias de um tipo
+
+function retornarSaldo(saldo:string | number){
+    return saldo
+}
+
+// no exemplo de cima a variavel saldo aceita tanto o tipo string como o tipo number
+
+console.log(retornarSaldo(5000))
+console.log(retornarSaldo('5000'))
+
