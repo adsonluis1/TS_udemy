ccfaa56ad453084ef3579bd76e55c66c1dc8bb56
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-10 18:47:05 -0300

    void.ts

---
+++ 4-funcoes_em_ts/js/void.js
@@ -0,0 +1,5 @@
+function showreturn() {
+    console.log('1,2,3 testando');
+    // return 9 da erro se retornaremos algo
+}
+showreturn();
---
+++ 4-funcoes_em_ts/void.ts
@@ -0,0 +1,6 @@
+function showreturn():void{
+    console.log('1,2,3 testando')
+    // return 9 da erro se retornaremos algo
+}
+
+showreturn()
