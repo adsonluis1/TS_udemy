2884c980436b8399bf4e01d37ea29261afdc6665
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-10 19:49:04 -0300

    aprofundando em generic operations

--- 4-funcoes_em_ts/generic_operations.js
+++ 4-funcoes_em_ts/generic_operations.js
@@ -7,3 +7,15 @@ function showcarros(carros) {
 }
 console.log(showcarros({ marca: 'BMW', modelo: 'm3', velMax: '300km/h' }));
 console.log(showcarros({ marca: 'Fiat', modelo: 'Touro', cargaMax: '1 tonelada', capota: 'sim' }));
+// reduzindo os tipos que podem ser usados no generic operations
+function maiorOUmenor(num1, num2) {
+    var numMaior;
+    if (+num1 > +num2) {
+        numMaior = num1;
+    }
+    else {
+        numMaior = num2;
+    }
+    console.log("o numero maior \u00E9 o ".concat(numMaior));
+}
+maiorOUmenor('60', '10');
--- 4-funcoes_em_ts/generic_operations.ts
+++ 4-funcoes_em_ts/generic_operations.ts
@@ -11,3 +11,16 @@ function showcarros<U>(carros:U):U{
 console.log(showcarros({marca:'BMW' , modelo:'m3' , velMax:'300km/h'}))
 console.log(showcarros({marca:'Fiat' , modelo:'Touro' , cargaMax:'1 tonelada' , capota:'sim'}))
 
+// reduzindo os tipos que podem ser usados no generic operations
+
+function maiorOUmenor<T extends string | number>(num1:T , num2:T){
+    let numMaior:T
+    if(+num1 > + num2){
+        numMaior = num1
+    }else{
+        numMaior = num2
+    }
+    console.log(`o numero maior é o ${numMaior}`)
+}
+
+maiorOUmenor('60', '10')
