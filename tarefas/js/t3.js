d361429dfa0d1c2d99909614ac8e163b3e2cc5df
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-10 18:38:21 -0300

    ajeitando as pastas

---
+++ tarefas/js/t3.js
@@ -0,0 +1,27 @@
+function analise(nota) {
+    if (typeof nota == 'number') {
+        switch (nota) {
+            case 1:
+                console.log('pessimo');
+                break;
+            case 2:
+                console.log('ruim');
+                break;
+            case 3:
+                console.log('mais ou menos');
+                break;
+            case 4:
+                console.log('bom, mas da pra melhorar');
+                break;
+            case 5:
+                console.log('otimo');
+                break;
+            default:
+                console.log('a nota tem que ser entre 1 e 5');
+        }
+    }
+    else {
+        console.log('Desculpe, você não deu a nota');
+    }
+}
+analise(5);
