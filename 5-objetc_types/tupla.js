a6fc84c975a12d2dcb99344e5cbdda475c84d86b
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-11 19:11:32 -0300

    tupla

---
+++ 5-objetc_types/tupla.js
@@ -0,0 +1,6 @@
+// tupla é uma especie de array que setamos a quantidade de elementos e os valores
+var usuario = ['adson', 'luis', 18];
+console.log(usuario);
+usuario[0] = 'anne';
+console.log(usuario);
+// as tuplas podem mudar o valor contando que sigam as regras setadas
