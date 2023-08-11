edd7fa142fb951cf54a5550c5cd4c3f1ad21a6d4
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-11 19:20:48 -0300

    tupla_com_readonly.ts

---
+++ 5-objetc_types/js/tupla_com_readonly.js
@@ -0,0 +1,5 @@
+function numbes(numbers) {
+    console.log(numbers[0]);
+    console.log(numbers[1]);
+}
+numbes([1, 3]);
---
+++ 5-objetc_types/tupla_com_readonly.ts
@@ -0,0 +1,6 @@
+function numbes (numbers: readonly [number, number]){
+    console.log(numbers[0])
+    console.log(numbers[1])
+}
+
+numbes([1,3])
