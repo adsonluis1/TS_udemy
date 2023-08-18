f05d5780e4932904bcb5a1dc03577235f643e409
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-18 19:45:23 -0300

    criando components

---
+++ 10_react_com_ts/src/components/primeiroComponets.tsx
@@ -0,0 +1,15 @@
+// 3- componente
+
+import React from 'react'
+
+type Props = {}
+
+function primeiroComponets({}: Props) {
+  return (
+    <div>
+        <h1>meu primeiro componente</h1>
+    </div>
+  )
+}
+
+export default primeiroComponets
