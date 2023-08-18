d02701986b8c894f6b750439c261fb12ae847ca9
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-18 19:26:53 -0300

    Variáveis no React

--- 10_react_com_ts/src/App.tsx
+++ 10_react_com_ts/src/App.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const nome:string= 'adson'
+const idade:number = 18
+const trabalhando:boolean = true
+
 function App() {
   return (
     <div className="App">
         <h1>Hello word React!</h1>
+        <h2>nome: {nome}</h2>
+        <h2>idade: {idade}</h2>
+        {trabalhando && <h2>trabalhando</h2>}
     </div>
   );
 }
