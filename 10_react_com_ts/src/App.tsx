0c4c6483f2a343d40ad8be3d1089ea197a802767
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-18 19:36:44 -0300

    funções react e ts

--- 10_react_com_ts/src/App.tsx
+++ 10_react_com_ts/src/App.tsx
@@ -2,17 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const nome:string= 'adson'
-const idade:number = 18
-const trabalhando:boolean = true
+
 
 function App() {
+  // 1- variaveis
+  const nome:string= 'adson'
+  const idade:number = 18
+  const trabalhando:boolean = true
+
+  //2-funcões
+
+  const showpessoa = (nome:string, idade:number)=>{
+    return `nome: ${nome}, idade: ${idade}`
+  }
+
+  //3-
+
   return (
     <div className="App">
         <h1>Hello word React!</h1>
         <h2>nome: {nome}</h2>
         <h2>idade: {idade}</h2>
         {trabalhando && <h2>trabalhando</h2>}
+
+        <h3>{showpessoa(nome,idade)}</h3>
+
     </div>
   );
 }
